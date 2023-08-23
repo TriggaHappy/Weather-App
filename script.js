7cbd97d77f33b627e354d00b15f77994b4032a1b
TriggaHappy <19maximilianmerten96@gmail.com>
2023-08-23 12:45:10 +0000

    added fnc for fetch/added temp display

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 'use strict';
 
+//HTML elements
 const btn = document.querySelector('[search]');
-const input = document.querySelector('[input]');
-const control = document.querySelector('[control]');
-let apiKey = 'eafb8fbf299373ccddfba8adbc96f97b';
+const inputEL = document.querySelector('[input]');
+const controlEL = document.querySelector('[control]');
+const temperatureEL = document.querySelector('[temp]');
+//Elements
 
-btn.addEventListener('click', function () {
+//variables
+let apiKey = 'eafb8fbf299373ccddfba8adbc96f97b';
+let icon = document.getElementById('icon');
+let temperatur = '';
+
+//functions
+btn.addEventListener('click', function fetch() {
   $.ajax({
     method: 'GET',
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}`,
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${inputEL.value}&appid=${apiKey}`,
     //headers: { 'X-Api-Key': 'YOUR_API_KEY'},
     //contentType: 'application/json',
-    success: function (result) {
-      console.log(result);
+    dataType: 'JSON',
+    success: function (data) {
+      console.log(data);
+      icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      temperatur = Math.trunc(Number(data.main.temp - 273, 15)); //dynamic!!!!
+      temperatureEL.textContent = temperatur;
     },
     error: function ajaxError(jqXHR) {
       console.error('Error: ', jqXHR.responseText);
     },
   });
 });
+
+//grid css / html
+//drop down menu -> units (Celsius, Farenheit, Standard)
+// sunrise sunset hint: unix timestamp
