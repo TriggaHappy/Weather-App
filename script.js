3cf05efe69f541f4868bf3eeb50b1462ceda1841
TriggaHappy <19maximilianmerten96@gmail.com>
2023-08-23 11:15:43 +0000

    added $.ajax fnc for api

--- script.js
+++ script.js
@@ -3,17 +3,19 @@
 const btn = document.querySelector('[search]');
 const input = document.querySelector('[input]');
 const control = document.querySelector('[control]');
-let city = '';
-
-
+let apiKey = 'eafb8fbf299373ccddfba8adbc96f97b';
 
 btn.addEventListener('click', function () {
-  city = input.value;
+  $.ajax({
+    method: 'GET',
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}`,
+    //headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+    //contentType: 'application/json',
+    success: function (result) {
+      console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+      console.error('Error: ', jqXHR.responseText);
+    },
+  });
 });
-
-let apiKey = 'eafb8fbf299373ccddfba8adbc96f97b';
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-
-api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey};
-
-fetch(queryURL)
