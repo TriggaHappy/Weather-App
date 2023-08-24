93ccd7fa10b0b1fe9de928c0869929681dd1aef7
TriggaHappy <19maximilianmerten96@gmail.com>
2023-08-24 13:14:46 +0000

    added timestamp sunrise, sunset not working

--- script.js
+++ script.js
@@ -4,16 +4,20 @@
 const btn = document.querySelector('[search]');
 const inputEL = document.querySelector('[input]');
 const controlEL = document.querySelector('[control]');
-const temperatureEL = document.querySelector('[temp]');
+const temperatureEL = document.querySelector('[tempSecond]');
+const sunriseEL = document.querySelector('[sunriseSecond]');
+const sunsetEl = document.querySelector('.[sunsetSecond]');
 //Elements
 
 //variables
 let apiKey = 'eafb8fbf299373ccddfba8adbc96f97b';
 let icon = document.getElementById('icon');
 let temperatur = '';
+let unixTimestampSunrise = '';
+let unixTimestampSunset = '';
 
 //functions
-btn.addEventListener('click', function fetch() {
+btn.addEventListener('click', function () {
   $.ajax({
     method: 'GET',
     url: `http://api.openweathermap.org/data/2.5/weather?q=${inputEL.value}&appid=${apiKey}`,
@@ -25,6 +29,19 @@ btn.addEventListener('click', function fetch() {
       icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       temperatur = Math.trunc(Number(data.main.temp - 273, 15)); //dynamic!!!!
       temperatureEL.textContent = temperatur;
+
+      //sunrise
+      unixTimestampSunrise = data.sys.sunrise * 1000;
+      let SunriseObject = new Date(unixTimestampSunrise);
+      let SunriseDateFormat = SunriseObject.toLocaleTimeString();
+      sunriseEL.textContent = SunriseDateFormat;
+
+      //sunset
+      unixTimestampSunset = data.sys.sunset * 1000;
+      let SunsetObject = new Date(unixTimestampSunset);
+      let SunsetDateFormat = SunsetObject.toLocaleTimeString();
+      console.log(SunsetDateFormat);
+      //sunsetEl.textContent = SunsetDateFormat; NOT WORKING
     },
     error: function ajaxError(jqXHR) {
       console.error('Error: ', jqXHR.responseText);
