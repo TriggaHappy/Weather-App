d5d85a9582e599c95b54d39cfe332edce81e4087
TriggaHappy <19maximilianmerten96@gmail.com>
2023-10-02 11:29:28 +0000

    unsure

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const sunriseEL = document.querySelector('[sunriseSecond]');
 const sunsetEL = document.querySelector('[sunsetSecond]');
 const dropdownContentEL = document.querySelector('[dropdown-content]');
 const dropdownValueEL = document.querySelector('[dropdown-value]');
-//Elements
+const inputError = document.querySelector('input[name="name"]');
 
 //variables
 let apiKey = 'eafb8fbf299373ccddfba8adbc96f97b';
@@ -18,12 +18,23 @@ let temperature = '';
 let unixTimestampSunrise = '';
 let unixTimestampSunset = '';
 let systemChosen = 'metric';
+let errorObj = {};
 
 //time Conversion
 function convertUnixTimeStampToLocalTime(time) {
   unixTimestampSunrise = time * 1000;
   let dateObject = new Date(unixTimestampSunrise);
-  return dateObject.toLocaleTimeString();
+  return dateObject.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+}
+
+//invalid Input`
+function Error(returnedData) {
+  if (returnedData === '400') {
+    alert('Nothing to geocode');
+  }
 }
 
 //calculation system
@@ -47,28 +58,35 @@ function temperatureCalculation(temp) {
 
 //API pull
 btn.addEventListener('click', function () {
-  $.ajax({
-    method: 'GET',
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${inputEL.value}&appid=${apiKey}&units=${systemChosen}`,
-    //headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-    //contentType: 'application/json',
-    dataType: 'JSON',
-    success: function (data) {
-      icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  if (inputEL.value !== '') {
+    $.ajax({
+      method: 'GET',
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${inputEL.value}&appid=${apiKey}&units=${systemChosen}`,
+      //headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+      //contentType: 'application/json',
+      dataType: 'JSON',
+      success: function (data) {
+        icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
-      //temperature textContent
-      temperatureCalculation(data.main.temp);
+        //temperature textContent
+        temperatureCalculation(data.main.temp);
 
-      //sunrise & sunset
-      sunriseEL.textContent = convertUnixTimeStampToLocalTime(data.sys.sunrise);
-      sunsetEL.textContent = convertUnixTimeStampToLocalTime(data.sys.sunset);
-      console.log(systemChosen);
-    },
+        //sunrise & sunset
+        sunriseEL.textContent = convertUnixTimeStampToLocalTime(
+          data.sys.sunrise
+        );
+        sunsetEL.textContent = convertUnixTimeStampToLocalTime(data.sys.sunset);
+      },
 
-    error: function ajaxError(jqXHR) {
-      console.error('Error: ', jqXHR.responseText);
-    },
-  });
+      error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+        errorObj = JSON.parse(jqXHR.responseText);
+        Error(errorObj.cod);
+      },
+    });
+  } else {
+    //alert("Input Field can't be empty!");
+  }
 });
 
 //grid css / html
